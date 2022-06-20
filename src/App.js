2bbb116a617f581d06e8523c0f7189dd4f95e2a7
Ruslanbori <57313184+Ruslanbori@users.noreply.github.com>
2022-06-20 16:19:01 +0300

    https://github.com/CodingWithRuslan/Memory_Game.git

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 
 import './App.css';
 
+const cardImages = [
+  {"src": "/img/helmet-1.png"},
+  {"src": "/img/potion-1.png"},
+  {"src": "/img/ring-1.png"},
+  {"src": "/img/scroll-1.png"},
+  {"src": "/img/shield-1.png"},
+  {"src": "/img/sword-1.png"}
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  
+  // shuffle cards
+  const shuffleCards = () => {
+    const shffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random() }))
+
+    setCards(shffledCards)
+    setTurns(0)
+
+  }
+  
+  console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
-        <button>New Game</button>
+        <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
